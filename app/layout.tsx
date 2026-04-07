594e32fafafcc7187a4f785e28ce8540d370fb31
Dziudita <Stakesheep.team@gmail.com>
2026-04-07 17:19:58 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,21 @@
 export default function RootLayout({ children }: any) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <style>{`
+          * {
+            box-sizing: border-box;
+          }
+
+          html, body {
+            margin: 0;
+            padding: 0;
+            background: #040405;
+          }
+        `}</style>
+
+        {children}
+      </body>
     </html>
   );
 }
